b8af44ac03414ae9765812877fa918538a27bbc9
Menmery <2053186460@qq.com>
2025-11-17 18:24:55 +0800

    1

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import router from './router'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* 2.路由绑定 */}
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
