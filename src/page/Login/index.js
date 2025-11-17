75c2b7cf24b51efaf771a9b8d8f4a0cc83758a7c
Menmery <2053186460@qq.com>
2025-11-17 19:15:56 +0800

    路由导航跳转

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,5 +1,16 @@
+import { Link, useNavigate } from "react-router-dom"
+
 const Login = () => {
-  return <div>Login</div>
+  const navigate = useNavigate()
+  return (
+    <div>
+      Login
+      {/* 声明式写法 */}
+      <Link to='/article'>跳转到article</Link>
+      {/* 命令式写法 */}
+      <button onClick={() => navigate('/article')}>跳转到article</button>
+    </div>
+  )
 }
 
 export default Login
