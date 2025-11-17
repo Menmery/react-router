bfbefb14781d092c9f5e184d8bfaf76ed384cd1d
Menmery <2053186460@qq.com>
2025-11-17 10:59:33 +0800

    最简单路由

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// 创建路由实例对象 并配置关系
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <div>登录页面</div>
+  },
+  {
+    path: '/article',
+    element: <div>文章页面</div>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
 
